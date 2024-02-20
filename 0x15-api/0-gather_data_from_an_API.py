5488858656e7344d6a696a6aaea545f9ab98a653
ArtaxJay <okjaceauthor@gmail.com>
2024-02-20 02:05:40 +0000

    Add Add 0-* file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,44 +1,15 @@
 #!/usr/bin/python3
-"""Gets todos from jsonplaceholder for a specified employee"""
+"""Gets todos from jsonplaceholder for a specified employee."""
 import requests
 import sys
 
-
-def get_user_info(user_id):
-    """Get user information based on user ID."""
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(url + "users/{}".format(user_id))
-    user_data = user_response.json()
-    return user_data
-
-
-def get_user_todos(user_id):
-    """Get completed tasks for a given user ID."""
-    url = "https://jsonplaceholder.typicode.com/"
-    todos_response = requests.get(url + "todos", params={"userId": user_id})
-    todos_data = todos_response.json()
-    """
-    completed_tasks = [todo.get("title") for todo in todos_data
-                       if todo.get("completed")]
-    """
-    return todos_data
-
-
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <user_id>")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    emps = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    td_lt = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    user_id = sys.argv[1]
-    user_info = get_user_info(user_id)
-    todos_data = get_user_todos(user_id)
-    completed_tasks = [todo.get("title") for todo in
-                       todos_data if todo.get("completed")]
-    total_tasks = len(completed_tasks) + len(todos_data)
-
-    print("Employee {} is done with tasks ({}/{}):".format(
-        user_info.get("name"), len(completed_tasks),
-        total_tasks))
-
-    for task in completed_tasks:
-        print("\t{}".format(task))
+    response = \
+        [td.get("title") for td in td_lt if td.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        emps.get("name"), len(response), len(td_lt)))
+    [print("\t {}".format(each)) for each in response]
