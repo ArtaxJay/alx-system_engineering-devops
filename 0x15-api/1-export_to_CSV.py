f3fd4da998ddb87c04fd8a699f1b061374cb57f9
ArtaxJay <okjaceauthor@gmail.com>
2024-02-20 09:38:17 +0000

    Modify 2-*

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,12 +5,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    emps = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    td_lt = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    emps = requests.get(url + "users/{}".format(user_id)).json()
+    td_lt = requests.get(url + "todos", params={"userId": user_id}).json()
+    emps_username = emps.get("username")
 
     with open(f"{user_id}.csv", 'w', newline='') as csv_doc:
         write_to_csv = csv.writer(csv_doc, quoting=csv.QUOTE_ALL)
         [write_to_csv.writerow(
-            [user_id, username, td.get('completed'), td.get('title')]
+            [user_id, emps_username, task.get('completed'), task.get('title')]
             ) for task in td_lt]
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """Gets todos from jsonplaceholder for a specified employee."""
+import json
 import requests
 import sys
 
 if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    emps = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    td_lt = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    emps = requests.get(url + "users/{}".format(user_id)).json()
+    td_lt = requests.get(url + "todos", params={"userId": user_id}).json()
+    emps_username = emps.get("username")
 
     with open(f"{user_id}.json", 'w') as json_file:
         """ Export data to a json file"""
         json.dump({user_id: [{
                 "task": td.get("title"),
                 "completed": td.get("completed"),
-                "username": username
+                "username": emps_username
                 } for td in td_lt]}, json_file)
