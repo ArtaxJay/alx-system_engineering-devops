c72f1ce77373914ae33e1ea78b9f051e79ad5cd4
ArtaxJay <132425606+ArtaxJay@users.noreply.github.com>
2024-08-01 03:09:26 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Find and fix a 5xx error when requesting a HTTP GET on an Apache web server
-
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
