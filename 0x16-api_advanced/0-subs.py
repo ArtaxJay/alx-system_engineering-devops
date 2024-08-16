e8deab771629c927da81d9653cda87013f1da138
ArtaxJay <okjaceauthor@gmail.com>
2024-08-16 04:04:10 +0100

    Write a script to query Reddit APIs for subreddit subscribers.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Use the Request module to get total Subreddit Subcribers from Reddit APIs."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """If successful, get total subscribers for a particular subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/artaxjay)"
+    }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 404:
+        return 0
+    results = res.json().get("data")
+    return results.get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,22 @@
+# ALX Advanced API Course
+
+## Title: 0x16. API advanced
+
+## Tags:
+
+- Python
+- Scripting
+- Back-end
+- API
+
+## Tasks' files:
+
+0. 0-subs.py
+1. 1-top_ten.py
+2. 2-recurse.py
+
+## Optional task's file:
+
+1. 100-count.py
+
+# Done by: ArtaxJay Oluwajuwon Olaolwa
