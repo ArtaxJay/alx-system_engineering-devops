8356f03ebf1f8ca0147af9994d1d1d2dddb0c54c
ArtaxJay <okjaceauthor@gmail.com>
2024-08-23 03:56:28 +0100

    Add a puppet file to config Nginx to increase the number of opened files it can handle at a time.

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# Optimize Nginx server to handle bulky opened files successfully
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
